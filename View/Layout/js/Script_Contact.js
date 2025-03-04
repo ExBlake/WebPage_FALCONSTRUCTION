f397dc80c915085c8418cd22783bccbc2d85671e
ExBlake <santi.v.a.v1999@gmail.com>
2025-03-04 16:04:45 -0500

    Se arreglo el copy del metodo SMS en contacto

--- View/Layout/js/Script_Contact.js
+++ View/Layout/js/Script_Contact.js
@@ -35,10 +35,13 @@ function sendMessage() {
         let mailtoLink = `mailto:commercial@falconstructs.com?subject=${encodeURIComponent(emailSubject)}&body=${emailBody}`;
         window.location.href = mailtoLink;
     } else if (contactMethod === "sms") {
-        let smsMessage = `Hello, my name is ${fullName}.%0A` +
-                         `Phone: ${phoneNumber}%0A` +
-                         `Message: ${message}`;
-        let smsURL = `sms:${phoneNumber}?body=${smsMessage}`;
+        let smsMessage = `Hello, my name is *${encodeURIComponent(fullName)}*.%0A%0A` +
+                         `I am very interested in your services and would love to have more details it.%0A%0A` +
+                         `Message: ${encodeURIComponent(message)}%0A%0A` +
+                         `Please give me a call at your earliest convenience.%0A`+
+                         `Phone: ${encodeURIComponent(phoneNumber)}%0A%0A` +
+                         `Looking forward to your response. Thank you!`;
+        let smsURL = `sms:+1904898800?body=${smsMessage}`;
         window.location.href = smsURL;
     }
 }
